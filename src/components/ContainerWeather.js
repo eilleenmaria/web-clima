421d8becd0d8940fc047fe151fe8b4653ec03639
eilleenmaria <egutierrezacosta85@gmail.com>
2024-11-01 16:45:54 -0500

    added comments containweather component

--- src/components/ContainerWeather.js
+++ src/components/ContainerWeather.js
@@ -13,10 +13,17 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const ContainerWeather = () => {
-    // let urlWeather = "https://api.openweathermap.org/data/2.5/weather?&appid=8463844bc7b8b6a16c856a476d162368&lang=es";
-    // let urlCity = "&q=";
-    // let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?&appid=8463844bc7b8b6a16c856a476d162368&lang=es";
+/**
+ * Componente que muestra el clima y el pronóstico para una ubicación específica.
+ *
+ * @component
+ * @example
+ * return (
+ *   <ContainerWeather />
+ * )
+ */
+
+const ContainerWeather = () => {    
     
     const [weather, setWeather] = useState([]);
     const [forecast, setForecast] = useState([]);
@@ -25,48 +32,59 @@ const ContainerWeather = () => {
     const [location, setLocation] = useState("");
     const [error, setError] = useState(""); 
     
+     /**
+     * Obtiene la ubicación del clima y el pronóstico basado en el nombre de la ciudad.
+     *
+     * @param {string} loc - Nombre de la ciudad para la que se busca el clima.
+     * @returns {Promise<void>} - Promesa que se resuelve cuando se completan las solicitudes.
+     */
     
     const getLocation = async(loc) => {
-        setLoading(true);
-        setLocation(loc);
-        setError("");
-        // Definir las URLs dentro de la función para reiniciar en cada llamada
-        const urlWeather = `https://api.openweathermap.org/data/2.5/weather?&appid=8463844bc7b8b6a16c856a476d162368&lang=es&q=${loc}`;
-        const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?&appid=8463844bc7b8b6a16c856a476d162368&lang=es&q=${loc}`;
+      setLoading(true);
+      setLocation(loc);
+      setError("");
 
-             
+     
+      const urlWeather = `https://api.openweathermap.org/data/2.5/weather?&appid=8463844bc7b8b6a16c856a476d162368&lang=es&q=${loc}`;
+      const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?&appid=8463844bc7b8b6a16c856a476d162368&lang=es&q=${loc}`;  
 
-    try {
-      const weatherResponse = await fetch(urlWeather);
-      if (!weatherResponse.ok) throw new Error('Ciudad no encontrada');
-      const weatherData = await weatherResponse.json();
-      setWeather(weatherData);
+      try {
 
-      const forecastResponse = await fetch(urlForecast );
-      if (!forecastResponse.ok) throw new Error('Se presento un error en la respuesta');
-      const forecastData = await forecastResponse.json();
-      setForecast(forecastData);
+        const weatherResponse = await fetch(urlWeather);
+        if (!weatherResponse.ok) throw new Error('Ciudad no encontrada');
+        const weatherData = await weatherResponse.json();
+        setWeather(weatherData);
 
-      setShow(true);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-      setShow(false);
-    }
-  };  
-    // Función para cerrar el mensaje de error
+        const forecastResponse = await fetch(urlForecast );
+        if (!forecastResponse.ok) throw new Error('Se presento un error en la respuesta');
+        const forecastData = await forecastResponse.json();
+        setForecast(forecastData);
+        setShow(true);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+        setShow(false);
+      }
+    };     
+    
+    /**
+     * Cierra el mensaje de error.
+     */
   const closeError = () => setError("");
 
+    /**
+     * Limpia los datos de clima y pronóstico.
+     */
   const clearData = () => {
     setWeather([]);
     setForecast([]);
     setShow(false);
-};
+  };
 
   return (
     <React.Fragment>
-        <FormSearchs newLocation = {getLocation} onClear={clearData}/>
+      <FormSearchs newLocation = {getLocation} onClear={clearData}/>
 
         {/* Renderiza el mensaje de error si existe */}
       {error && (
@@ -74,29 +92,29 @@ const ContainerWeather = () => {
           className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4"
           role="alert"
         >
-          <strong className="font-bold">¡Error!</strong>
-          <span className="block sm:inline">{error}</span>
-          <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={closeError}>
-            <svg
-              className="fill-current h-6 w-6 text-red-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
+        <strong className="font-bold">¡Error!</strong>
+        <span className="block sm:inline">{error}</span>
+        <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={closeError}>
+          <svg
+            className="fill-current h-6 w-6 text-red-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"  
+          >  
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
           </span>
         </div>
       )}
         <WeatherTable
-         showData = {show}
-         loadingData = {loading}
-         weather = {weather}
-         forecast = {forecast}/>
+        showData = {show}
+        loadingData = {loading}
+        weather = {weather}
+        forecast = {forecast}/>
 
-         {/* Renderiza el mapa si se ha cargado el clima con coordenadas */}
-         {show && weather && weather.coord && (
+        {/* Renderiza el mapa si se ha cargado el clima con coordenadas */}
+        {show && weather && weather.coord && (
                 <MapContainer
                     key={`${weather.coord.lat}-${weather.coord.lon}`} // Clave para forzar re-renderizado
                     center={[weather.coord.lat, weather.coord.lon]}
@@ -113,7 +131,7 @@ const ContainerWeather = () => {
                         </Popup>
                     </Marker>
                 </MapContainer>
-            )}
+        )}
     </React.Fragment>
   )
 }
