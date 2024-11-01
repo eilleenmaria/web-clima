6211852682614f2e9c996b19e551475385955500
eilleenmaria <egutierrezacosta85@gmail.com>
2024-10-31 19:47:06 -0500

    modified file

--- src/components/FormSearchs.js
+++ src/components/FormSearchs.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react'
     
-const FormSearchs = () => {
-    const[ciudad, setCiudad] = useState("");
-
+const FormSearchs = ({newLocation}) => {
+  
+    const[city, setCity] = useState("");
+    const [error, setError] = useState("");
+    
 const onSubmit = (evento)=>{
   evento.preventDefault();
-  console.log({ciudad});
-  if(ciudad === ''|| !ciudad)return;
+  console.log({city});
+  if (!city) {
+    setError("No haz ingresado datos. Lo sentimos, intenta nuevamente.");
+    return;
 }
+setError("");
+  newLocation(city);
+}
+ // Función para cerrar el mensaje de error
+ const closeError = () => setError("");
   return (
-    <div className="w-full max-w-lg min-w-[200px] relative">
+    <div className="w-full  max-w-lg min-w-[200px] p-8">
             <form onSubmit={onSubmit}>
             <div className="relative flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
@@ -19,7 +28,7 @@ const onSubmit = (evento)=>{
                 <input
                 className="w-full bg-transparent font-bold placeholder:text-gray-50 text-gray-50 text-sm border border-gray-50 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-gray-50 hover:border-gray-50 shadow-sm focus:shadow"
                 placeholder="Ingresa una ciudad..." 
-                onChange={(event)=>setCiudad(event.target.value)}
+                onChange={(event)=>setCity(event.target.value)}
                 />
                 
                 <button
@@ -30,8 +39,32 @@ const onSubmit = (evento)=>{
                 </button> 
             </div>
             </form>
+            {/* Renderiza el mensaje de error si existe */}
+{error && (
+  <div
+    className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4"
+    role="alert">
+    <strong className="font-bold">¡Error!</strong>
+    <span className="block sm:inline">{error}</span>
+    <span className="absolute top-0 bottom-0 right-0 px-4 py-3"  onClick={closeError}>
+      <svg
+        className="fill-current h-6 w-6 text-red-500"
+        role="button"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20">
+        <title>Close</title>
+        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+      </svg>
+    </span>
+  </div>
+)};
         </div>
   )
+
+
+
+
+
 }
 
 export default FormSearchs
