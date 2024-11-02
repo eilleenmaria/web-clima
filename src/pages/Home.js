5b1223e9aa3fba3d00b3478340d2629e2b399393
eilleenmaria <egutierrezacosta85@gmail.com>
2024-11-01 19:46:27 -0500

    modified file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ContainerWeather from '../components/ContainerWeather';
 
 const Home = () => {
-
   return (
     // Contenedor principal 
     <div className='relative  w-full h-screen mx-auto bg-neutral-300'>
